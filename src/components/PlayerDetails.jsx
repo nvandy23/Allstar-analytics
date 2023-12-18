a7f31bcfb264aef417b7a6f05dd425d7b69411ca
nvandy23 <nvandy2373@gmail.com>
2023-12-18 16:17:12 -0500

    test player details endpoint and render a single value

--- src/components/PlayerDetails.jsx
+++ src/components/PlayerDetails.jsx
@@ -1,34 +1,34 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-export default function PlayerDetails (){
-    const [player, setPlayer] = useState(null);
+export default function PlayerDetails() {
+  const [player, setPlayer] = useState(null);
 
-    async function getPlayerDetails (){
-      const url = `http://lookup-service-prod.mlb.com/json/named.player_info.bam?sport_code='mlb'&player_id=${player_id}`
+  async function getPlayerDetails() {
+    const url = `http://lookup-service-prod.mlb.com/json/named.player_info.bam?sport_code='mlb'&player_id='493316'`;
 
-try {
-	const response = await fetch(url);
-	const result = await response.json();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      setPlayer(result);
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getPlayerDetails();
+  }, []);
+
+  return (
+    <div>
+      <h1>{player?.player_info?.queryResults?.row.name_display_first_last
+}</h1>
+    </div>
+  );
 }
 
-        }
-        useEffect(() => {
-            getPlayerDetails();
-          }, []);
-
-  //const handleChange = (e) =>{
-
- // }
-        return (
-        <h1>Player details</h1>
-        )
-
-        
-    }
   
 
 
