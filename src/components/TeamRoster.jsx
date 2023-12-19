1ec2e66c56ce72f0323b79178007a450398016f4
nvandy23 <nvandy2373@gmail.com>
2023-12-19 18:50:15 -0500

    setup player roster API, use states, and render player rosters

--- src/components/TeamRoster.jsx
+++ src/components/TeamRoster.jsx
@@ -1,28 +1,21 @@
-import { useState, useEffect } from "react";
+import React from "react";
 
-export default function TeamRoster() {
-  const [teamRoster, setTeamRoster] = useState(null);
-
-  async function getTeamRoster() {
-    const url = `http://lookup-service-prod.mlb.com/json/named.roster_team_alltime.bam?start_season='2016'&end_season='2017'&team_id='121'`;
-
-    try {
-      const response = await fetch(url);
-      const result = await response.json();
-      setTeamRoster(result);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  useEffect(() => {
-    getTeamRoster();
-  }, []);
-
+const TeamDetailsPage = ({ teamDetails, selectedStartSeason, selectedEndSeason }) => {
   return (
     <div>
-      <h1>{teamRoster?.roster_team_alltime?.queryResults?.row[0]?.name_last_first}</h1>
+      <h1>Team Details</h1>
+      {teamDetails ? (
+        <div>
+          <h2>{teamDetails.name_display_full}</h2>
+          <p>Start Season: {selectedStartSeason}</p>
+          <p>End Season: {selectedEndSeason}</p>
+        </div>
+      ) : (
+        <p>Loading team details...</p>
+      )}
     </div>
   );
-}
+};
+
+export default TeamDetailsPage;
 
