94687dd0017503df2048078daa8ce61f0af9d6b3
nvandy23 <nvandy2373@gmail.com>
2023-12-20 10:07:27 -0500

    test player stats details endpoint working

--- src/components/PlayerStatsDetails.jsx
+++ src/components/PlayerStatsDetails.jsx
@@ -2,11 +2,11 @@ import {useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function PlayerStats () {
-const {id} =useParams()
+// const {id} =useParams()
 const [playerStats, setPlayerStats] =useState(null)
 const [playerSeason,setPlayerSeason] =useState("2017")
  async function getPlayerStats () {
-    const url = `http://lookup-service-prod.mlb.com/json/named.sport_hitting_tm.bam?league_list_id='mlb'&game_type='R'&season=${playerSeason}&player_id='${id}`
+    const url = "http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id='493316'"
     try {
         const response = await fetch(url);
         const result = await response.json();
